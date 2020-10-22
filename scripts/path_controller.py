9ef09ee5ff329b356d215fed46d5df30de890b11
ricardogrando <ricardogrando13@gmail.com>
2020-10-22 14:35:27 -0300

    23v42v3

--- scripts/path_controller.py
+++ scripts/path_controller.py
@@ -26,10 +26,10 @@ def path_callback(data):
 	index_x = int(pose.position.x*map_resolution)
 	index_y = int(pose.position.y*map_resolution)
 
-	if (index_x < 0): index_x = 0
-	if (index_x > _map.shape[0]): index_x = _map.shape[0]
-	if (index_x < 0): index_x = 0
-	if (index_x > _map.shape[0]): index_x = _map.shape[0]
+	if (index_x < 1): index_x = 1
+	if (index_x > _map.shape[0]-1): index_x = _map.shape[0]-1
+	if (index_y < 1): index_y = 1
+	if (index_y > _map.shape[0]-1): index_y = _map.shape[0]-1
 
 	if (_map[_map.shape[0] - index_y][index_x] == 1):
 		_map[_map.shape[0] - index_y][index_x] = 2
@@ -54,9 +54,15 @@ if __name__ == "__main__":
 	velocity = Twist()
 	while not rospy.is_shutdown():
 		# FACA SEU CODIGO AQUI
-
-		velocity.linear.x = 0.0
-		pub.publish(velocity)
+		if (len(laser.ranges) > 0):
+			if (min(laser.ranges[90:270]) > 0.25):
+				velocity.linear.x = random.uniform(-0.1, -0.25)
+				velocity.angular.z = 0.0
+			else:
+				velocity.linear.x = 0.0
+				velocity.angular.z = 0.25
+			
+			pub.publish(velocity)
 				
 		r.sleep()
 
